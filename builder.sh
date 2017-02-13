4496d4b08a9d25afe43dbc8405c2fa85b92f757f
NMT <thitu@users.noreply.github.com>
2017-02-13 13:12:49 -0500

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -6,6 +6,4 @@ if [ "$tag" = "" ]; then
   tag="latest"
 fi
 
-docker -t thitu/android-sdk:$tag -f Dockerfile .
-docker push thitu/android-sdk:$tag
-
+docker build -t thitu/android-sdk:$tag -f Dockerfile . && docker push thitu/android-sdk:$tag
