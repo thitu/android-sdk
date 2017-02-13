ebde39bdfb448588bb00d37e3bf8de79179a2c42
NMT <thitu@users.noreply.github.com>
2017-02-13 13:12:31 -0500

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -6,6 +6,4 @@ if [ "$tag" = "" ]; then
   tag="jumbo"
 fi
 
-docker -t thitu/android-sdk:$tag -f Dockerfile .
-docker push thitu/android-sdk:$tag
-
+docker build -t thitu/android-sdk:$tag -f Dockerfile . && docker push thitu/android-sdk:$tag
